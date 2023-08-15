3dc2c80290c7846c5dcc71e466b98f296aea1a07
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-08-15 23:30:46 +0600

    1. write a program tin Java to input 5 numbers from the keyboard and find their sum and average.java

--- Lab2_14_08_2023/1. write a program tin Java to input 5 numbers from the keyboard and find their sum and average.java
+++ Lab2_14_08_2023/1. write a program tin Java to input 5 numbers from the keyboard and find their sum and average.java
@@ -1,19 +1,35 @@
 import java.util.Scanner;
-public class Problem1 {
+
+public class Main {
     public static void main(String[] args) {
-        float num;
-        float sum = 0;
-        float average=0;
-       
+        int choiceInput;
         Scanner sc = new Scanner(System.in);
-        for (int i=1; i<=5; i++){
-            System.out.print("Enter the number  = ");
-            num = sc.nextFloat();
-             sum = sum + num;
-             average = sum/5;
-        }
+
+        do {
+            float num;
+            float sum = 0;
+            float average = 0;
+
+            for (int i = 1; i <= 5; i++) {
+                System.out.print("Enter the number = ");
+                num = sc.nextFloat();
+                sum = sum + num;
+                average = sum /i;
+            }
 
             System.out.println("Sum: " + sum);
-            System.out.println("Average : "+average);
+            System.out.println("Average: " + average);
+
+            do {
+                System.out.print("Do you want to continue? (1=Yes, 2=Exit): ");
+                choiceInput = sc.nextInt();
+                if (choiceInput != 1 && choiceInput != 2) {
+                    System.out.println("Invalid input. Please enter 1 to continue or 2 to exit.");
+                }
+            } while (choiceInput != 1 && choiceInput != 2);
+
+        } while (choiceInput == 1);
+
+        sc.close();
     }
 }
