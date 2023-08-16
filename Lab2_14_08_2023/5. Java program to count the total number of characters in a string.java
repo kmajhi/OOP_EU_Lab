fcd9875cef99f8faa2b3fb953cfb8347bfc814d1
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-08-16 23:37:55 +0600

    5. Java program to count the total number of characters in a string.java

--- Lab2_14_08_2023/5. Java program to count the total number of characters in a string.java
+++ Lab2_14_08_2023/5. Java program to count the total number of characters in a string.java
@@ -8,3 +8,36 @@ public class Main {
         System.out.println("The total number of characters in the string is: " + inputCharacter.length());
     }
 }
+
+
+//using getters and setters
+/*
+
+import java.util.Scanner;
+class GetterAndSetter{
+    String name;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
+public class Main {
+    public static void main(String[] args) {
+        GetterAndSetter name = new GetterAndSetter();
+        System.out.print("Enter the name: ");
+        Scanner sc = new Scanner(System.in);
+        String inputName = sc.nextLine();
+        name.setName(inputName);
+        System.out.println();
+
+        System.out.println("Name is, " + name.getName()+"\nThe total number of characters in the string is: "+ name.getName().length());
+
+
+    }
+}
+
+*/
