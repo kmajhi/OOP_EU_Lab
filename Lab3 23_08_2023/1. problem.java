fd89f0e69b5ece70a4bd956d57eab7b0c91bd8c4
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-08-23 10:47:11 +0600

    1. problem.java

---
+++ Lab3 23_08_2023/1. problem.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Problem6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Enter the name = ");
+            String name = sc.nextLine();
+            System.out.println("Entered name is: " + name);
+
+            int inputNumber;
+            do {
+                System.out.print("Do you want to continue? (1 for Yes / 2 for exit): ");
+                while (!sc.hasNextInt()) {
+                    System.out.println("Invalid input. Please enter a valid integer.");
+                    sc.nextLine();
+                }
+                inputNumber = sc.nextInt();
+                sc.nextLine();
+
+                if (inputNumber == 1) {
+                    break;
+                } else if (inputNumber == 2) {
+                    System.out.println("Question test passed. Thanks for coding.");
+                    sc.close();
+                    return;
+                } else {
+                    System.out.println("Invalid input. Please enter 1 to continue or 2 to exit.");
+                }
+            } while (true);
+        }
+    }
+}
