10afd560a637da22ace656d22bdc1caf542fe502
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-10-11 10:59:15 +0600

    3. Problem.java

---
+++ Lab_After_Mid/Lab1 11.10.2023/3. Problem.java
@@ -0,0 +1,14 @@
+package overloading;
+
+public class CheckRectangle {
+    CheckRectangle(int x){
+        System.out.println("This is a square");
+    }
+    CheckRectangle(int a, int b, int c, int d){
+        System.out.println("It is a simple rectangle.");
+    }
+
+    public static void main(String[] args) {
+        CheckRectangle verify = new CheckRectangle(3,3,3,3);
+    }
+}
